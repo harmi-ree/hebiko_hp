2836f939e8a7b8198bc68c3b365ca52d0f601a20
harumi <hrm.goo29@gmail.com>
2023-04-16 01:11:56 +0900

    add google map

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <header>
-        <section>
-          <div></div>
-        </section>
-      </header>
       <main>
+        <header class="article">
+          <div class="section_header">
+            <div class="section_header_container">
+              <div id="hamburger">
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </header>
         <div class="article">
           <div class="section" id="sec01">
-            <div class="section_container"></div>
+            <div class="section_container">
+              <p>やさしいへび子</p>
+              <p>おいしいおにぎりやいなり寿司を作っています。</p>
+            </div>
           </div>
           <div class="section" id="sec02">
             <h2>Section2</h2>
@@ -22,9 +29,27 @@ function App() {
             <h2>Section3</h2>
             <p>ここは3番目のコンテンツです。</p>
           </div>
-          <div class="section" id="sec04">
-            <h2>Section4</h2>
-            <p>ここは4番目のコンテンツです。</p>
+          <div class="section_shop_info" id="sec04">
+            <div class="section_container">
+              <h2>店舗情報</h2>
+              <h4>住所</h4>
+              <p>大阪府松原市阿保6-1-20</p>
+              <div class="iframe_wrap">
+                {/* <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d205.3063085705473!2d135.5572390692526!3d34.5813730670089!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000d971710d4213%3A0xfb8c7e6a334fe0df!2z44KE44GV44GX44GE44G444Gz5a2Q!5e0!3m2!1sja!2sjp!4v1681570345240!5m2!1sja!2sjp"
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                  class="googleMap"
+                ></iframe> */}
+                <iframe
+                  src="https://snazzymaps.com/embed/481681"
+                  width="400px"
+                  height="400px"
+                  class="googleMap"
+                ></iframe>
+              </div>
+            </div>
           </div>
         </div>
 
